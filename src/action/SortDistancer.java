1bdf6fa6a934dd38b95c4b8d0c342a044743f5be
ahntuann <ahntuan284@gmail.com>
2024-09-11 13:48:12 +0700

    New code in 11/09

--- src/action/SortDistancer.java
+++ src/action/SortDistancer.java
@@ -1,26 +1,28 @@
 package action;
 
 import checker.DistanceCalculator;
-import jsclub.codefest2024.sdk.model.Element;
 import jsclub.codefest2024.sdk.model.players.Player;
+import myClass.PlayerAndDistance;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class SortDistancer {
-    public static List<Player> sorter(List<Player> elements, Player player) {
+    public static List<PlayerAndDistance> sorter(List<Player> elements, Player player) {
+        List<PlayerAndDistance> result = new ArrayList<>();
+
         List<Integer> dists = new ArrayList<>();
         int n = elements.size();
         for (int i = 0; i < n; i++) {
+            result.add(new PlayerAndDistance(null, 0));
+
             Player e = elements.get(i);
 
-            dists.set(i, DistanceCalculator.calc(e, player));
+            dists.add(DistanceCalculator.calc(e, player));
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n - i + 1; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
                 if (dists.get(i) > dists.get(j)) {
                     int tmp = dists.get(i);
                     Player tmpE = elements.get(i);
@@ -34,7 +36,11 @@ public class SortDistancer {
             }
         }
 
-        return elements;
+        for (int i = 0; i < n; i++) {
+            result.set(i, new PlayerAndDistance(elements.get(i), dists.get(i)));
+        }
+
+        return result;
     }
 
 }
---
+++ src/myClass/PlayerAndDistance.java
@@ -0,0 +1,13 @@
+package myClass;
+
+import jsclub.codefest2024.sdk.model.players.Player;
+
+public class PlayerAndDistance {
+    public Player player;
+    public int dis;
+
+    public PlayerAndDistance(Player players, int dis) {
+        this.player = players;
+        this.dis = dis;
+    }
+}
